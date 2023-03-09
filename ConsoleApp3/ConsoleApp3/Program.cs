f6bdb06758adcd19730bc941a04adf556e27b7c1
Dima <sdvsvd11@gmail.com>
2023-03-09 23:48:46 +0200

    3task finish

---
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        List<string> products = File.ReadAllLines("C:/1/11.txt").ToList();
+
+        var groupedProducts = products.GroupBy(p => GetCategory(p));
+
+        foreach (var group in groupedProducts)
+        {
+            Console.WriteLine("Category: " + group.Key);
+            foreach (var product in group)
+            {
+                Console.WriteLine("- " + product);
+            }
+            Console.WriteLine();
+        }
+    }
+    static string GetCategory(string productName)
+    {
+        if (productName.ToLower().Contains("молочнi"))
+        {
+            return "Молочнi";
+        }
+        else if (productName.ToLower().Contains("м'яснi"))
+        {
+            return "М'яснi";
+        }
+        else if (productName.ToLower().Contains("овоч"))
+        {
+            return "Овочi та фрукти";
+        }
+        else
+        {
+            return "Інші";
+        }
+    }
+}
